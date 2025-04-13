e668521590c9ec2afd24544e776ec0a211ef6911
Andrei Makarov <aomak@yandex.ru>
2025-04-13 19:23:26 +0300

    fixture begin

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,4 +2,12 @@ import pytest
 
 @pytest.fixture
 def user_input_correct():
-    card_mask = "Visa Platinum 7000 79** **** 6361"
+    return "Visa Platinum 7000 79** **** 6361"
+
+
+@pytest.fixture
+def filter_executed():
+    return [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
+]
---
+++ tests/test_processing.py
@@ -0,0 +1,6 @@
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+from tests.conftest import filter_by_state, sort_by_date
+
