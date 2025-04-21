5a6ea9947bcfc5df46b6daf0bfd2917cb339ec2b
Andrei Makarov <aomak@yandex.ru>
2025-04-21 19:03:15 +0300

    commit 2

--- src/decorators.py
+++ src/decorators.py
@@ -1,19 +1,25 @@
 import time
-from datetime import datetime
+from functools import wraps
+
+
+from pyexpat.errors import messages
+
 
 def logging_time(filename = None):
     def wrapper(func):
+        @wraps(func)
         def inner(*args, **kwargs):
             t1 = time.localtime()
             time_begin = time.strftime("%H:%M:%S", t1)
             result = func(*args, **kwargs)
             t2 = time.localtime()
             time_end = time.strftime("%H:%M:%S", t2)
+            message_ok = f"{func} Время начала работы {time_begin}, время конца работы {time_end}"
             if filename == None:
-                print(f"Время начала работы {time_begin}, время конца работы {time_end}")
+                print(message_ok)
             else:
                 with open(filename, 'w', encoding = "utf8") as file:
-                    file.write(f"Время начала работы {time_begin}, время конца работы {time_end}")
+                    file.write(message_ok)
             return result
 
         return inner
