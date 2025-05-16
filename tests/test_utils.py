62971a992e3e25204ee79defc88fde3abbbc0c99
Andrei Makarov <aomak@yandex.ru>
2025-05-16 17:25:57 +0300

    homework 12.1 test 1

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,83 +1,8 @@
 import pytest
 
+from src.utils import get_transactions
 
-@pytest.fixture
-def filter_executed():
-    return [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    ]
-
-
-@pytest.fixture
-def filter_canceled():
-    return [
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ]
-
-
-@pytest.fixture
-def sorted_date():
-    return [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    ]
-
-
-@pytest.fixture
-def currency_eur():
-    return [
-        {
-            "id": 142264287,
-            "state": "CANCELED",
-            "date": "2019-05-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79614.93",
-                "currency": {"name": "EUR", "code": "EUR"},
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188",
-        }
-    ]
-
-
-@pytest.fixture
-def currency_usd():
-    return [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {"name": "USD", "code": "USD"},
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {"name": "USD", "code": "USD"},
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188",
-        },
-    ]
-
-
-@pytest.fixture
-def list_operation():
-    return [
+list_oper = [
   {
     "id": 441945886,
     "state": "EXECUTED",
@@ -1570,3 +1495,17 @@ def list_operation():
     "to": "Счет 96527012349577388612"
   }
 ]
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        ("data/operations.json", list_oper),
+        ("dksjfljfsfdfsfs", []),
+        ("data/empty.json", []),
+        ("data/not_list.json", [])
+    ],
+)
+def test_get_transaction(value, expected):
+    assert get_transactions(value) == expected
+
+
