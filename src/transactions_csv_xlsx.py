0b7f6e79ae6aede4227e9218d40fa4ec5df50dc3
Andrei Makarov <aomak@yandex.ru>
2025-05-25 17:08:21 +0300

    homework_13_1

--- src/transactions_csv_xlsx.py
+++ src/transactions_csv_xlsx.py
@@ -1,13 +1,12 @@
 import pandas as pd
 
+
 def get_transactions_from_csv(filepath: str) -> list[dict]:
     try:
         df = pd.read_csv(filepath).to_dict(orient="records")
     except Exception as ex:
         raise Exception(f"Возникла ошибка {ex}")
     return df
-
-# print(get_transactions_from_csv("data/transactions.csv"))
 
 
 def get_transactions_from_xlsx(filepath: str) -> list[dict]:
@@ -16,10 +15,3 @@ def get_transactions_from_xlsx(filepath: str) -> list[dict]:
     except Exception as ex:
         raise Exception(f"Возникла ошибка {ex}")
     return df
-
-# print(get_transactions_from_xlsx("data/transactions_excel1.xlsx"))
-
-
-
-
-
