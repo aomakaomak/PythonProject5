ac7319f19f6faf6cc4a57ca2354ba3608e6c9c56
Andrei Makarov <aomak@yandex.ru>
2025-06-01 15:06:14 +0300

    homework_13_2_task_1_v1

--- src/search_description.py
+++ src/search_description.py
@@ -7,63 +7,70 @@ def search_in_description(transactions: list[dict], string: str) -> list[dict]:
     """
     pattern = re.compile(fr"{string}")
     new_transactions = []
-    for transaction in transactions:
-       if re.search(pattern, transaction["description"]):
-           new_transactions.append(transaction)
+    try:
+        for transaction in transactions:
+            if re.search(pattern, transaction["description"]):
+                new_transactions.append(transaction)
+    except Exception as ex:
+          raise KeyError(f"Возникла ошибка {ex}")
+
+
+
+
     return new_transactions
 
 
 
 
 
-my_transactions = [
-  {
-    "id": 441945886,
-    "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-      "amount": "31957.58",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Оплата за коммуналку",
-    "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
-  },
-  {
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "2019-07-03T18:35:29.512364",
-    "operationAmount": {
-      "amount": "8221.37",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод физическому лицу",
-    "from": "MasterCard 7158300734726758",
-    "to": "Счет 35383033474447895560"
-  },
-  {
-    "id": 939719570,
-    "state": "EXECUTED",
-    "date": "2018-06-30T02:08:58.425572",
-    "operationAmount": {
-      "amount": "9824.07",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    },
-    "description": "Перевод организации для различных нужд",
-    "from": "Счет 75106830613657916952",
-    "to": "Счет 11776614605963066702"
-  }
-]
-
-result = search_in_description(my_transactions, "Перевод")
-
-print(result)
+# my_transactions = [
+#   {
+#     "id": 441945886,
+#     "state": "EXECUTED",
+#     "date": "2019-08-26T10:50:58.294041",
+#     "operationAmount": {
+#       "amount": "31957.58",
+#       "currency": {
+#         "name": "руб.",
+#         "code": "RUB"
+#       }
+#     },
+#     "description": "Оплата за коммуналку",
+#     "from": "Maestro 1596837868705199",
+#     "to": "Счет 64686473678894779589"
+#   },
+#   {
+#     "id": 41428829,
+#     "state": "EXECUTED",
+#     "date": "2019-07-03T18:35:29.512364",
+#     "operationAmount": {
+#       "amount": "8221.37",
+#       "currency": {
+#         "name": "USD",
+#         "code": "USD"
+#       }
+#     },
+#     "description": "Перевод физическому лицу",
+#     "from": "MasterCard 7158300734726758",
+#     "to": "Счет 35383033474447895560"
+#   },
+#   {
+#     "id": 939719570,
+#     "state": "EXECUTED",
+#     "date": "2018-06-30T02:08:58.425572",
+#     "operationAmount": {
+#       "amount": "9824.07",
+#       "currency": {
+#         "name": "USD",
+#         "code": "USD"
+#       }
+#     },
+#     # "description": "Перевод организации для различных нужд",
+#     "from": "Счет 75106830613657916952",
+#     "to": "Счет 11776614605963066702"
+#   }
+# ]
+#
+# result = search_in_description(my_transactions, "Перевод")
+#
+# print(result)
